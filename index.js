60b19a3e90f56728479efb6b2e89bb9c85d63e6f
Abdulazizkhan001 <abdualzizbotirxonov93@gmail.com>
2021-10-31 14:55:09 +0500

    nested looop

--- index.js
+++ index.js
@@ -1,65 +1,129 @@
-       // SQRT
- function kvadrat (a, b){
-   return a**b;
-   }
- console.log(kvadrat(5, 2));
+// var title = 'WebBrain academy'.repeat(5);
+// title.toLocaleLowerCase();
+// var a = 0;
+// for (var i = 0; i <= title.length; i++) {
+//     if (title[i] === 'a') {
+//         a++
+//     }
+// }
+// console.log(a + ` ${'ta a harfi'}`);
 
-       // CBRT
-function kub (a,b){
-   return a**b*a;
-   }
- console.log(kub(5, 2))
+// var b = 0;
+// for (var i = 0; i <= title.length; i++) {
+//     if (title[i] === 'b') {
+//         b++
+//     }
+// }
+// console.log(b + ` ${'ta b harfi'}`);
 
-       // ROUND 
-function roundCopy(a){
-  console.log(a.toFixed(0))
+// for (let i = 0; i <= 100; i++) {
+//     if (i % 2 == 0) {
+//         console.log(i);
+//     }
+// }
+
+// for(let i = 0;i <= 100; i++)
+// {
+//     if( i % 2 !== 0)
+//     {
+//         console.log(i);
+//     }
+// }
+
+// for( let i = 10; i>= 0; i--)
+// {
+//     console.log(i);
+// }
+
+// var number = '748394736384937483';
+// var juft = 0;
+// for (var i = 0; i < number.length; i += 2) {
+//     juft += Number(number[i]);
+// }
+// console.log(juft);
+
+// var toq = 0;
+// for (var i = 1; i < number.length; i += 2) {
+//     toq += Number(number[i]);
+// }
+// console.log(toq);
+
+// var text = 'WebBrain academy'.repeat(5);
+// for (i = 0; i <= text.length; i++) {
+//   text=  text.replace(/\s+/g, '')
+// }
+// console.log(text);
+
+
+// var data = [
+//     {
+//         name: 'John',
+//         friends: [
+//             {
+//                 name: 'Zokirxon',
+//             },
+//             {
+//                 name: 'Abdurauf',
+//             },
+//             {
+//                 name: 'Otabek',
+//             }
+//         ],
+//     },
+//     {
+//         name: 'John',
+//         friends2: [
+//             {
+//                 name: 'Zokirxon',
+//             },
+//             {
+//                 name: 'Abdurauf',
+//             },
+//             {
+//                 name: 'Otabek',
+//             }
+//         ],
+//     },
+//     {
+//         name: 'John',
+//         friends3: [
+//             {
+//                 name: 'Zokirxon',
+//             },
+//             {
+//                 name: 'Abdurauf',
+//             },
+//             {
+//                 name: 'Otabek',
+//             }
+//         ],
+//     }
+// ]
+
+// for( var i = 0; i < data.length;i++)
+// {
+//     for(var j = 0;j < friends.length;j++)
+//     {
+//         console.log(data[0].friends2)
+//     }
+// }
+
+var isCorrect = () => {
+    var arr = ["matiz", "matiz", "matiz", "matiz"]
+    let count = 0;
+    for (var i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[j] == arr[i]) {
+                count++;
+            }
+        }
+        if(count == arr.length)
+        {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 }
-roundCopy(52.3);
-
-       // CEIL  
-function ceilCopy(a){
-  var qoldiq = a % 1 * 10;
-  console.log(qoldiq >= 5 ? a + 1 - 0.: console.log());
-}
-ceilCopy(25369.325)
-
-       // FLOOR 
-let floor = (a) => {
-  var qoldiq = a % 2;
-  console.log(qoldiq < 9 ? parseInt(a) : parseInt(a));
-}  
-floor(15.5)
-
- let weCounter = 0; 
- let aCounter = 0; 
- let bCounter = 0; 
-
-  var cW = 'Welcome Webbrain'.repeat(5);
-  var sanoq =() => {
-   var index = cW.indexOf('We')
-    weCounter++;
-    cW = cW.slice (index+2)
-    cW.includes('We') && sanoq()   
-  } 
-  sanoq();
-  console.log(weCounter)
-  
-  var cW2 = 'Welcome Webbrain'.repeat(5)
-  var aL =() => {
-    var index = cW2.indexOf('a')
-     aCounter++;
-     cW2 = cW2.slice (index+1)
-     cW2.includes('a') && aL()   
-   } 
-   aL();
-   console.log(aCounter);
-  
-   var cW3 = 'Welcome Webbrain'.repeat(5)
-  var bL =() => {
-    var index = cW3.indexOf('b')
-     bCounter++;
-     cW3 = cW3.slice (index+1)
-     cW3.includes('b') && bL()   
-   } 
-   bL();
-   console.log(bCounter);
+console.log(isCorrect()); 
